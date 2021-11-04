0a518f3026e54336858aa8a8a0a7fd9ae6e7483b
wConnor <connorga4@gmail.com>
2021-11-04 21:51:22 +0000

    added test package

--- project/src/test/java/com/devops/groupb/harbourmaster/TideTest.java
+++ project/src/test/java/com/devops/groupb/harbourmaster/TideTest.java
@@ -1,3 +1,5 @@
+package com.devops.groupb.harbourmaster.test;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,19 +16,19 @@ import java.util.Arrays;
 
 @Transactional
 public class TideTest {
-	private transient final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(this.getClass());    
+	private transient final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(this.getClass());
 
 	@Autowired
-    private TideRepository tideRepository;
+	private TideRepository tideRepository;
 
 	@Transactional
-    @Test
-    public void getAllTides() {
+	@Test
+	public void getAllTides() {
 		log.info("Testing if any tides are present in the database.");
 		List<Tide> tides = tideRepository.findAll();
 
 		log.info("Got tides " + Arrays.toString(tides.toArray()) + ".");
-		
-        assertFalse(tides.isEmpty());
+
+		assertFalse(tides.isEmpty());
 	}
 }
