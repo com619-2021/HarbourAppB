4cc52e504d8b08e88979e0d36d545947e787ea22
wConnor <connorga4@gmail.com>
2021-11-24 00:13:49 +0000

    started looking at test problems; new scheduling system conflicts.
    tests may need a rewrite. orders attempt to schedule the pilot but
    NullPointer is returned when adding a pilot to the pilots array needed
    for the order object. may need to use OrderService.findSuitablePilot
    instead of manually constructing an array..?

--- project/src/test/java/com/devops/groupb/harbourmaster/service/GPSServiceTest.java
+++ project/src/test/java/com/devops/groupb/harbourmaster/service/GPSServiceTest.java
@@ -67,24 +67,21 @@ public class GPSServiceTest {
 		Pilot pilot = new Pilot(allowedTo, "Alex", "Wilbert", LocalDate.of(1985, Month.FEBRUARY, 15), workingHours);
 		Berth berth = new Berth(53.25, -2.6);
 
-		/* placing the order for the gpsService to find; allocatedTime manually
-		   set to ensure that the ship is in the permitted time range (2hrs before) */
-		Order order = new Order(ship, berth, LocalDate.now().plusDays(1L));
+		/* placing the order for the gpsService to find. */
+		Order order = new Order(ship, berth, LocalDate.of(2021, 11, 24));
 		List<Pilot> pilots = new ArrayList<Pilot>();
 		pilots.add(pilot);
-		order.setAllocatedTime(LocalDateTime.now().minusHours(1L));
 		orderService.placeOrder(order, pilots);
 
-		/* pinging is dependent on a random number generator, so a while loop
+		/* pinging is dependent on a random number generator, so a for loop
 		   is necessary to repeatedly check for a ship to finally appear. null
 		   implies that the ship has not appeared or it is out of the permitted time range.*/
 		GPS gps = gpsService.pingPresence(order.getShip().getUUID());
-		int i = 0;
 		/* failing to find in 100 attempts implies problem with GPS or having
 		   extremely bad luck. */
-		while (gps == null && i != 100) {
+		int i = 0;
+		for (; gps == null && i != 100; ++i) {
 			gps = gpsService.pingPresence(ship.getUUID());
-			++i;
 		}
 
 		log.info("GPS for a legal ship after " + i + " pings: " + gps);
@@ -95,19 +92,16 @@ public class GPSServiceTest {
 		/* generates a new ship UUID to avoid conflicts with the previous test.
 		   this ship should not come up on the GPS as they are too early. */
 		Ship ship2 = new Ship(ShipType.CARGO, 1.3);
-		Order order2 = new Order(ship2, berth, LocalDate.now());
-
-		order.setAllocatedTime(LocalDateTime.now().minusHours(2L));
+		Order order2 = new Order(ship2, berth, LocalDate.of(2021, 11, 24));
 		orderService.placeOrder(order2, pilots);
 
 		GPS gps2 = gpsService.pingPresence(ship.getUUID());
-		int i2 = 0;
-		while (gps2 == null && i2 != 20) {
+		i = 0;
+		for (; gps2 == null && i != 20; ++i) {
 			gps2 = gpsService.pingPresence(ship.getUUID());
-			++i2;
 		}
 
-		log.info("GPS for an early ship after " + i2 + " pings: " + gps2);
+		log.info("GPS for an early ship after " + i + " pings: " + gps2);
 		assertNull(gps2);
 	}
 
@@ -131,8 +125,7 @@ public class GPSServiceTest {
 		List<Pilot> pilots = new ArrayList<Pilot>();
 		pilots.add(pilot);
 
-		Order order = new Order(ship, berth, LocalDate.now().plusDays(1L));
-		order.setAllocatedTime(LocalDateTime.now().minusHours(1L));
+		Order order = new Order(ship, berth, LocalDate.of(2021, 11, 24));
 		orderService.placeOrder(order, pilots);
 
 		GPS gps = gpsService.pingPresence(order.getShip().getUUID());
